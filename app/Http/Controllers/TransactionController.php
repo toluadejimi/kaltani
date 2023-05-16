855210bd4139b548ab51b95f597787a13dff6d49
Jimmy <toluadejimi@gmail.com>
2023-05-16 10:55:38 +0100

    fhfhh

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -61,72 +61,29 @@ class TransactionController extends Controller
     public function verify_bank_account(Request $request)
     {
 
-        $account_number = $request->account_number;
-        $bank_code = $request->bank_code;
+        $account_number = $request->input('account_number');
+        $bank_code = $request->input('bank_code');
 
-        $databody = array(
-            "account_number" => $account_number,
-            "account_bank" => $bank_code,
-
-        );
-
-        $body = json_encode($databody);
         $curl = curl_init();
-
-        $key = env('FLW_SECRET_KEY');
-        //"Authorization: $key",
-        curl_setopt($curl, CURLOPT_URL, 'https://api.flutterwave.com/v3/accounts/resolve');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_ENCODING, '');
-        curl_setopt($curl, CURLOPT_MAXREDIRS, 10);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 0);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt(
-            $curl,
-            CURLOPT_HTTPHEADER,
-            array(
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://enkpayweb.enkwave.com/api/resolve-bank?account_number=$account_number&bank_code=$bank_code",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_HTTPHEADER => array(
                 'Content-Type: application/json',
-                'Accept: application/json',
-                "Authorization: $key",
-            )
-        );
-
+            ),
+        ));
+    
         $var = curl_exec($curl);
         curl_close($curl);
-        $result = json_decode($var);
 
-        if ($result->data == null) {
-
-            $message = $result->message;
-
-            return response()->json([
-                'status' => $this->FailedStatus,
-                'message' => $message,
-            ], 500);
-
-        }
-
-        $acc_name = $result->data->account_name;
-        $message = $result->message;
-
-        if ($result->status == 'success') {
-
-            return response()->json([
-                'status' => $this->SuccessStatus,
-                'account_name' => $acc_name,
-
-            ], 200);
-
-        }
-
-        return response()->json([
-            'status' => $this->FailedStatus,
-            'message' => $message,
-        ], 500);
+        $var = json_decode($var);
+        return response()->json(['status' => $this->SuccessStatus, 'message' => $var], 200);
 
     }
 
@@ -187,6 +144,10 @@ class TransactionController extends Controller
         return response()->json(['status' => $this->SuccessStatus, 'message' => $var], 200);
 
     }
+
+
+
+
 
 
 
