4784dc3012f18e881aef7a6288bf5f10a682cabf
Jimmy <toluadejimi@gmail.com>
2025-07-26 10:33:59 +0100

    pudhh

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public $message;
+
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    public function broadcastOn()
+    {
+        // Public channel "chat"
+        return new Channel('chat');
+    }
+
+    public function broadcastAs()
+    {
+        return 'MessageSent';
+    }
+}
