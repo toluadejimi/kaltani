5be9972899e8bcb46361b8681ed4f41f43af9343
Jimmy <toluadejimi@gmail.com>
2025-07-26 17:30:39 +0100

    pudhh

--- app/Http/Controllers/BroadcastAvailableDrivers.php
+++ app/Http/Controllers/BroadcastAvailableDrivers.php
@@ -20,14 +20,14 @@ class BroadcastAvailableDrivers extends Controller
 
         $drivers = User::where('status', 1)->get(['id', 'first_name', 'last_name', 'latitude', 'longitude', 'status']);
         if($drivers){
-            broadcast(new AvailableDrivers($drivers))->toOthers();
+            return response()->json([
+                'status' => true,
+                'data' => $drivers
+            ]);
 
         }
 
-        return response()->json([
-            'status' => true,
-            'message' => 'Available drivers broadcast'
-        ]);
+
     }
 
 }
