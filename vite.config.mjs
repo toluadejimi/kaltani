db2201e22ceb5af840c8fe6ef627f37646fd24e6
Jimmy <toluadejimi@gmail.com>
2025-07-26 15:01:01 +0100

    pudhh

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,31 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel([
+//             'resources/css/app.css',
+//             'resources/js/app.js',
+//         ]),
+//     ],
+// });
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
 export default defineConfig({
-    plugins: [laravel({
-        input: ['resources/js/app.js'],
-        refresh: true,
-    })],
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
     server: {
-        host: '0.0.0.0',
+        host: 'kaltanimis.com',  // your dev hostname
         port: 5173,
+        strictPort: true,
+        cors: true,
     },
 });
+
+
