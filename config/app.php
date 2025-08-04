efff2cc97f3183f14b5df3b55de17ad8899afe6d
toluadejimi <toluadejimi@gmail.com>
2025-08-05 00:54:41 +0100

    pushh

--- config/app.php
+++ config/app.php
@@ -181,6 +181,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
+
 
         /*
          * Package Services Providers...
